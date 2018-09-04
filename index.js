9fe1bc2ba64120978f25fd188d1a9d2d8c58a544
Benjamin Yocum <bbyocum@verisk.com>
2018-09-04 16:31:56 -0600

    Added http-server dependency, renamed site.html

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const {Builder, By} = require('selenium-webdriver');
 
 (async function example() {
 	let browser = await new Builder()
-		.usingServer('http://localhost:5555')
+		.usingServer('http://localhost:4444/wd/hub')
 		.forBrowser('internet explorer')
 		.build();
 	try {
-		await browser.get('http://127.0.0.1:8080/site.html');
+		await browser.get('http://127.0.0.1:8080/index.html');
 		await browser.findElement(By.id('openPopup')).click();
 
 		let handles = await browser.getAllWindowHandles();
